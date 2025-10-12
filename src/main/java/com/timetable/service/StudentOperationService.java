fb507726ea7d82e261db1f822f36378c7d53c072
sgzs <sgzs6721@gmail.com>
2025-10-12 11:41:50 +0800

    fix: Replace List.of with Collections.singletonList for student names assignment

--- src/main/java/com/timetable/service/StudentOperationService.java
+++ src/main/java/com/timetable/service/StudentOperationService.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Collections;
 
 @Service
 public class StudentOperationService {
@@ -43,7 +44,7 @@ public class StudentOperationService {
         StudentAliasDTO alias = new StudentAliasDTO();
         alias.setCoachId(coachId);
         alias.setAliasName(request.getAliasName());
-        alias.setStudentNames(List.of(request.getOldName()));
+        alias.setStudentNames(Collections.singletonList(request.getOldName()));
         
         Long id = studentAliasRepository.save(alias);
         alias.setId(id);
