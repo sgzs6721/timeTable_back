72062caafa188edc2985e5b0030866af2b4bd818
sgzs <sgzs6721@gmail.com>
2025-10-13 18:16:58 +0800

    refactor: 精简微信登录请求DTO，移除未使用的属性和构造函数

--- src/main/java/com/timetable/dto/WechatLoginRequest.java
+++ src/main/java/com/timetable/dto/WechatLoginRequest.java
@@ -1,42 +1,13 @@
 package com.timetable.dto;
 
-/**
- * 微信登录请求DTO
- */
 public class WechatLoginRequest {
-    
     private String code;
-    
-    private String state;
-    
-    public WechatLoginRequest() {}
-    
-    public WechatLoginRequest(String code, String state) {
-        this.code = code;
-        this.state = state;
-    }
-    
+
     public String getCode() {
         return code;
     }
-    
+
     public void setCode(String code) {
         this.code = code;
-    }
-    
-    public String getState() {
-        return state;
-    }
-    
-    public void setState(String state) {
-        this.state = state;
-    }
-    
-    @Override
-    public String toString() {
-        return "WechatLoginRequest{" +
-                "code='" + code + '\'' +
-                ", state='" + state + '\'' +
-                '}';
     }
 }
