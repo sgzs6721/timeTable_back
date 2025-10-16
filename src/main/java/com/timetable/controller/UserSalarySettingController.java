6c0e4b2bfbb4618ca99ce67e896cc23415956855
sgzs <sgzs6721@gmail.com>
2025-10-16 23:57:57 +0800

    fix: 修正工资设置接口的请求路径前缀，去掉/api

--- src/main/java/com/timetable/controller/UserSalarySettingController.java
+++ src/main/java/com/timetable/controller/UserSalarySettingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/salary-settings")
+@RequestMapping("/salary-settings")
 public class UserSalarySettingController {
 
     @Autowired
