d85d9b3afcb151ccd047674ec22ef7aa2be21c51
sgzs <sgzs6721@gmail.com>
2025-10-21 12:00:56 +0800

    refactor: 修改客户控制器的请求映射路径，从"/api/customers"更改为"/customers"，以简化API结构

--- src/main/java/com/timetable/controller/CustomerController.java
+++ src/main/java/com/timetable/controller/CustomerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("/customers")
 @CrossOrigin(origins = "*")
 public class CustomerController {
 
